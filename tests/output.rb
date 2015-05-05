82bb9e2cacbd32c7838ddb2ec5827a6ba08f8c13
Giuseppe Caponetto <giuseppegi.caponetto@studenti.unicam.it>
2015-05-05 19:52:32 +0200

    updated output.rb

--- tests/output.rb
+++ tests/output.rb
@@ -1,9 +1,10 @@
 require "micromidi"
 
 # This example sends some messages to an output selected by the user
+
 output = UniMIDI::Output.gets
 
-MIDI.using(output) do
+MIDI.using(output) do 
   note "C5"
   cc 5, 120
   play "C3", 3
