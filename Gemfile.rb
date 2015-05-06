ed33431c8eeb8647823d26fb272807d83c853f2c
Giuseppe Caponetto <giuseppegi.caponetto@studenti.unicam.it>
2015-05-06 15:33:51 +0200

    updated gem list

--- Gemfile.rb
+++ Gemfile.rb
@@ -2,3 +2,6 @@ source 'https://rubygems.org'
 
 gem 'midi-message'
 gem 'unimidi'
+gem 'midi-topaz'
+gem 'midi-eye'
+gem 'micromidi'
--- examples/one-shot-note.rb
+++ examples/one-shot-note.rb
@@ -1,8 +1,22 @@
 require 'midi-message'
 require 'unimidi'
+require 'topaz'
 
 #asks the user to select the midi driver
-output = UniMIDI::Output.gets
+#output = UniMIDI::Output.gets
 
-message = MIDIMessage::NoteOn.new(0, 64, 64)
-output.puts(message)
+#selects the first availapble midi driver
+
+@output = UniMIDI::Output.use(:first) 
+
+@duration = 0.5 #in seconds
+
+@message_1_on = MIDIMessage::NoteOn.new(0, 64, 64)
+@message_1_off = MIDIMessage::NoteOff.new(0, 64, 64)
+def play
+@output.puts(@message_1_on)
+sleep(@duration)
+@output.puts(@message_1_off)
+end
+
+play
