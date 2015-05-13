8248d5f97e94fdb6ff041d4415359554c009a3d0
Giuseppe Caponetto <giuseppegi.caponetto@studenti.unicam.it>
2015-05-13 12:57:12 +0200

    unit tests added

---
+++ lib/Pattern.rb
@@ -0,0 +1,12 @@
+
+
+class Pattern
+  attr_reader :steps_nr, :channel_nr
+  
+  def initialize(steps_number, channel_number)
+    @channel_nr = channel_number
+    @steps_nr = steps_number
+    @pattern = Array.new 
+  end
+  
+end
